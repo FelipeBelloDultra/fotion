ec1c162e1c5fba26ad592c9a29e9fae5e5c3139d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-12-15 18:59:46 -0300

    feat: redirect with page session is empty in authenticated layout

--- src/app/(authorized)/layout.tsx
+++ src/app/(authorized)/layout.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { nextAuthOptions } from "@/lib/next-auth/config";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { redirect } from "next/navigation";
 
 export default async function AuthorizedLayout({
   children,
@@ -11,15 +12,19 @@ export default async function AuthorizedLayout({
 }>) {
   const session = await getServerSession(nextAuthOptions);
 
+  if (!session || !session.user) {
+    return redirect("/home");
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar.Root
         navUser={
           <AppSidebar.NavUser
             user={{
-              name: session?.user?.name || "",
-              email: session?.user?.email || "",
-              avatar: session?.user?.image || undefined,
+              name: session.user.name || "",
+              email: session.user.email || "",
+              avatar: session.user.image || undefined,
             }}
           />
         }
